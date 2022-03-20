cc7e8ed3a2c28caa09a5ab3dcaf1fe1b01ee9b11
Niraj Limbu <nlimbu@yahoo.com>
2022-03-20 14:58:34 -0400

    download application as a phone app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
 require('bootstrap');
-const img1 = require('../../assets/img/food-table.jpg');
-const img2 = require('../../assets/img/grill.jpg');
+const img1 = require("../../assets/img/food-table.jpg");
+const img2 = require("../../assets/img/grill.jpg");
 
-$(document).ready(function () {
+$(document).ready(function() {
   // First image is hard coded in index.html
   const carouselSlides = [
     {
-      title: 'We travel all over the US',
-      subtitle: 'Check out our schedule!',
+      title: "We travel all over the US",
+      subtitle: "Check out our schedule!",
       img: img1,
-      btnText: 'View Schedule',
-      btnUrl: 'schedule.html',
+      btnText: "View Schedule",
+      btnUrl: "schedule.html"
     },
     {
-      title: 'Our food is seriously the bomb!',
-      subtitle: 'What are you waiting for?',
+      title: "Our food is seriously the bomb!",
+      subtitle: "What are you waiting for?",
       img: img2,
-      btnText: 'Purchase Tickets',
-      btnUrl: 'tickets.html',
+      btnText: "Purchase Tickets",
+      btnUrl: "tickets.html"
     },
   ];
-
+  
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
@@ -39,6 +39,6 @@ $(document).ready(function () {
           </div>
         </div>
     </div>
-  </div>`);
-  });
+  </div>`)
+  })
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
-// const WebpackPwaManifest = require("webpack-pwa-manifest");
+// const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 const config = {
@@ -20,7 +20,7 @@ const config = {
       directory: path.join(__dirname, '/'),
     },
     compress: true,
-    port: 9000,
+    port: 8080,
   },
   module: {
     rules: [
@@ -54,20 +54,23 @@ const config = {
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
     }),
-    // new WebpackPwaManifest({
-    //   name: "Food Event",
-    //   short_name: "Foodies",
-    //   description: "An app that allows you to view upcoming food events.",
-    //   background_color: "#01579b",
-    //   theme_color: "#ffffff",
-    //   fingerprints: false,
-    //   inject: false,
-    //   icons: [{
-    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
-    //     sizes: [96, 128, 192, 256, 384, 512],
-    //     destination: path.join("assets", "icons")
-    //   }]
-    // })
+    new WebpackPwaManifest({
+      name: 'Food Event',
+      short_name: 'Foodies',
+      description: 'An app that allows you to view upcoming food events.',
+      start_url: './index.html',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve('assets/img/icons/icon-512x512.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
   ],
   mode: 'development',
 };
